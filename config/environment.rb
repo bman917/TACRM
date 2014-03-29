80fef1fc900fa737be02f665fcfeb7e1de9c8e1d
Mr Jacky Chan <bman917@gmail.com>
2014-03-30 02:10:31 +0800

    moved common rails ext to a gem

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 TACRM::Application.initialize!
-
-Time::DATE_FORMATS[:time] = "%B %d, %I:%M %p"
-Time::DATE_FORMATS[:med] = "%b %e, %Y"
-Time::DATE_FORMATS[:long] = "%B %e, %Y"
