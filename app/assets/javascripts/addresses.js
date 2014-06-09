e35c1b4e628b280db4ca3d61905e4e98e8d7a4f1
Mr Jacky Chan <bman917@gmail.com>
2014-06-10 02:42:36 +0800

    1.6.7 fixes

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -1,12 +1,25 @@
 function init_address_form_container() {
+  $('div.country_not_found select').removeClass('optional');
+  $('div.country_not_found select').addClass('required');
+
+
   /*Add validation listener*/
   $('form#new_address_form').submit(address_form_submit_validation);
+
 }
 
-function address_form_submit_validation() {
+function address_form_submit_validation(event) {
 
   isFormValid = true;
   
+  $($("select.required").get().reverse()).each(function(){
+      if ($.trim($(this).val()).length == 0){
+          $(this).effect('shake');
+          $(this).focus();
+          isFormValid = false;
+      }
+  });
+
   $($("input.required").get().reverse()).each(function(){
       if ($.trim($(this).val()).length == 0){
           $(this).effect('shake');
@@ -14,6 +27,7 @@ function address_form_submit_validation() {
           isFormValid = false;
       }
   });
+
 
 
 
