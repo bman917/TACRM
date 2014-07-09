42e9a9cfb06650154c4dbda1aaa7aebcd29c0738
Mr Jacky Chan <bman917@gmail.com>
2014-07-10 01:48:45 +0800

    set passport expirty to 60 days

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,6 +1,6 @@
 class PortalController < ApplicationController
   def index
-    @expiring_passports = Identification.passports.where("expiration_date < ?", Date.today.to_date + 30)
+    @expiring_passports = Identification.passports.where("expiration_date < ?", Date.today.to_date + 60)
 
   end
 end
