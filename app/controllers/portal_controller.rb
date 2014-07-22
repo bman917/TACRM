c3c33a6067b4243cd3b35ae4100544c091d529a5
Mr Jacky Chan <bman917@gmail.com>
2014-07-23 00:25:19 +0800

    1.9 slautations, passport expiration

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,6 +1,6 @@
 class PortalController < ApplicationController
   def index
-    @expiring_passports = Identification.passports.where("expiration_date < ?", Date.today.to_date + 60)
+    @expiring_passports = Identification.passports.where("expiration_date < ?", Date.today.to_date + 210)
 
   end
 end
