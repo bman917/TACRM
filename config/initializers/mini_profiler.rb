e1d0cfa0f5da979d0fa84c69da7d4162415fcd22
bman917 <bman917@gmail.com>
2015-09-04 19:27:09 +0800

    turn off profiler

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,4 +1,4 @@
 #if %w(development staging production).include?(Rails.env)
-if %w(development staging test production).include?(Rails.env)
+if %w(development staging test production amayra_prod).include?(Rails.env)
   Rack::MiniProfiler.config.pre_authorize_cb = lambda {|env| false } 
 end
